5493e0c3bab212f957acf73a04e49af35adc4c63
dev-nullified <3087935+dev-nullified@users.noreply.github.com>
2020-01-21 16:25:14 -0800

    Implemented useful properties for Assignment 2

--- problem3-part2.py
+++ problem3-part2.py
@@ -1,14 +1,18 @@
 class Car:
-    def __init__(self):
-        pass
+    def __init__(self, color=None, nickName=None, owner=None):
+        self.color = color
+        self.nickName = nickName
+        self.owner = owner
 
     def main():
         pass
 
 
 class Bicycle:
-    def __init__(self):
-        pass
+    def __init__(self, color=None, nickName=None, owner=None):
+        self.color = color
+        self.nickName = nickName
+        self.owner = owner
 
     def main():
         pass
